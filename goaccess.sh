27e72ff925d50ebdffa6c84e95f5f6a431c4385b
jbar <jeanjacquesbrucker@gmail.com>
2020-12-29 11:23:15 +0000

    workaround for strange bug in goaccess.sh:
    When called by cron, goaccess didn't parse *.gz file. Strange. Is it because cron close stdin ??
    (which means no script may use the pipe while launched by cron ???)

--- goaccess.sh
+++ goaccess.sh
@@ -13,8 +13,8 @@ cd "$(dirname "$0")"
 mkdir -p "public/goaccess"
 
 fw="public/goaccess/$(date "+%Gw%V")-$(basename "$PWD").html"
-
-[ "$(find "$fw"  -cmin -60)" ] || zcat "$ACCESSLOG"*.gz | goaccess $(find "$ACCESSLOG"* -not -name "*.gz") --log-format=COMBINED -a -o "$fw"
+set -x
+[ "$(find "$fw"  -cmin -60)" ] || goaccess $(find "$ACCESSLOG"* -not -name "*.gz") <(zcat "$ACCESSLOG"*.gz) --log-format=COMBINED -a -o "$fw"
 
 goaccess $(find "$ACCESSLOG"* -not -name "*.gz") --log-format=COMBINED -a -o public/goaccess/last.html
 

0a4e7d04e057c7de2e215b68b850ce4c6efccee0
jbar <jeanjacquesbrucker@gmail.com>
2024-07-03 00:24:52 +0000

    create update_lastmod.sh

---
+++ update_lastmod.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+me="$(basename "$0")"
+cd "$(dirname "$0")"
+
+git restore --staged -- .
+
+find "${@:-content/}" -type f -name "*.md" | while read file ; do
+	
+	read h lastmod etc < <( grep -in -m 1 "^lastmod: \+[0-9-]\{10\}T[0-9:]\{8\}.[0-9:]\{5\}" "$file" )
+	lastauthordate="$(git log -1 --pretty="format:%aI" "$file")"
+	[[ "$lastauthordate" ]] || lastauthordate=$(date --iso-8601=seconds)
+
+	if [[ -z "$lastmod" ]] ; then
+		sed -i "1a\Lastmod: $lastauthordate # MANAGED BY ${me}" "$file"
+	else
+		sed -i "${h%%:*}c\Lastmod: $lastauthordate # MANAGED BY ${me}" "$file"
+	fi
+	if git diff --quiet -- "$file" ; then
+		echo "${me}: unchanged ${file}" >&2
+		continue
+	fi
+	continue
+	git add -- "$file"
+	git commit --date=${lastauthordate} --message="${me}: $file" --message="$lastmod -> $lastauthordate"
+done
